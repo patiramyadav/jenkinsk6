746f13f7a73ba3bc8cfb957c23215a62e5e56a0e
patiramyadav <patiram.yadav990@gmail.com>
2022-03-29 20:19:53 -0500

    install k6

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+export const options = {
+  vus: 5,
+  duration: '20s',
+};
+
 export default function () {
   http.get('https://test.k6.io');
   sleep(2);
